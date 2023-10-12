36cf666a11db47b017c63501c6a5e026431eb246
NicolasPozzer <pozzernico@gmail.com>
2023-10-12 01:05:53 -0300

    proyecto configurado, clases Done!

---
+++ src/app/components/tiket/tiket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tiket',
+  templateUrl: './tiket.component.html',
+  styleUrls: ['./tiket.component.css']
+})
+export class TiketComponent {
+
+}
